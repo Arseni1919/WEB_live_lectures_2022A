f688a55c3005106fd345c99fa5ab74a6b526f964
Arseni <1919ars@gmail.com>
2021-12-15 16:45:28 +0200

    (more)

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, redirect, url_for, render_template
-
+from flask import request
+from flask import session
 
 app = Flask(__name__)
+app.secret_key = '12345'
 
 
 @app.route('/hello/world')
@@ -21,6 +23,13 @@ def about_func():
 
 @app.route('/catalog')
 def catalog_func():
+    if 'user_inside' in session:
+        if session['user_inside']:
+            print('user inside')
+    if 'product' in request.args:
+        product = request.args['product']
+        size = request.args['size']
+        return render_template('catalog.html', p_name=product, p_size=size)
     return render_template('catalog.html')
 
 
@@ -35,6 +44,30 @@ def home_func():
     else:
         return render_template('index.html')
 
+
+@app.route('/logout')
+def logout_func():
+
+    session['username'] = ''
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_func():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # DB
+        found = True
+        if found:
+            session['username'] = username
+            session['user_inside'] = True
+            return redirect(url_for('home_func'))
+        else:
+            return render_template('login.html')
 
 
 if __name__ == '__main__':
